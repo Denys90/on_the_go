30a9e811577bf98f6ff0089be215c4c272109654
Denys90 <112502591+Denys90@users.noreply.github.com>
2024-04-22 20:04:41 +0000

    Deploying to gh-pages from @ Denys90/on_the_go@99fe339ee6badcb002cfe9a0bdcf1e75b843e09a 🚀

---
+++ assets/AboutPage-affd9abc.js
@@ -0,0 +1,24 @@
+import{n as s,t as i,j as e}from"./index-b4f5d619.js";const a=s.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 20px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+`,o=s.div`
+  display: block;
+  width: 250px;
+  border-radius: 10px;
+  padding: 15px;
+  box-shadow: ${i.colors.boxShadow};
+`,t=s.h2`
+  font-size: 21px;
+  margin-bottom: 10px;
+  color: ${i.colors.black};
+`,r=s.p`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: 0%;
+  text-align: left;
+  color: ${i.colors.gray};
+`,n=()=>e.jsxs(e.Fragment,{children:[e.jsx("div",{}),e.jsxs(a,{children:[e.jsxs(o,{children:[e.jsx(t,{children:"Convenient reservation system"}),e.jsx(r,{children:"You can easily book a camper online by choosing the date, duration and other additional options. It is fast, convenient and reliable."})]}),e.jsxs(o,{children:[e.jsx(t,{children:"Support and service"}),e.jsx(r,{children:"If you have any questions or need help, there is usually a support team on the site ready to help you. Also, after the rental, there may be support services during the trip, so that you have a calm and comfortable trip."})]}),e.jsxs(o,{children:[e.jsx(t,{children:"Full description of transport"}),e.jsx(r,{children:"Each camper has a detailed description with all the amenities it has. You can check the size, specifications, photos and user reviews before making a decision."})]}),e.jsxs(o,{children:[e.jsx(t,{children:"Affordable prices"}),e.jsx(r,{children:"The site offers competitive prices for renting camper vans. You can find options for different budgets, allowing you to plan your trip without worrying about the costs."})]}),e.jsxs(o,{children:[e.jsx(t,{children:"A wide choice of transport"}),e.jsx(r,{children:"The site allows you to choose a camper or motor home that best suits your needs and budget. From compact campers for a couple to large motorhomes for a large family or friends."})]})]})]}),l=()=>e.jsx(n,{});export{l as default};
---
+++ assets/FavoritesPage-6dd782c7.js
@@ -0,0 +1,84 @@
+import{n as s,t as e,r as p,j as o}from"./index-b4f5d619.js";import{u as h,b as g,s as a,d as f,g as b,M as j,h as v}from"./useAdvert-a5a8955a.js";const m=s.li`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  background-color: #fff;
+  border-radius: 12px;
+  padding: 20px;
+  margin-bottom: 20px;
+  margin-top: 40px;
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);
+  transition:
+    transform 0.3s,
+    box-shadow 0.3s;
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 12px 24px rgba(0, 0, 0, 0.2);
+  }
+
+  img {
+    width: 300px;
+    border-radius: 12px;
+    transition:
+      transform 0.3s,
+      box-shadow 0.3s;
+    margin-right: 20px;
+
+    &:hover {
+      box-shadow: 0 12px 24px rgba(0, 0, 0, 0.2);
+    }
+  }
+`,u=s.h2`
+  color: ${e.colors.black};
+  margin-bottom: 8px;
+  font-size: 24px;
+  font-weight: 700;
+`,w=s.div`
+  display: flex;
+`,y=s.button`
+  position: absolute;
+  top: 208px;
+  right: 20px;
+  font-size: 18px;
+  color: ${e.colors.black};
+  border: none;
+  background-color: ${e.colors.white};
+
+  &:hover {
+    color: ${e.colors.red};
+    text-shadow: ${e.colors.btnShadow};
+    transform: scale(1.1);
+  }
+`,k=s.p`
+  display: block;
+  font-size: 36px;
+  color: ${e.colors.black};
+  &::before {
+    content: '\\20AC';
+    display: inline-block;
+  }
+`,$=s.div`
+  display: flex;
+  margin-bottom: 100px;
+`,F=s.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`,S=s.h1`
+  margin-top: 40px;
+`,R=s.div`
+  display: flex;
+  margin-bottom: 8px;
+`;s.div`
+  /* display: block;
+  justify-content: flex-start; */
+`;const B=s.div`
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+`,C=s.div`
+  display: flex;
+  align-items: center;
+`,_=()=>{const[n,l]=p.useState(!1),{favorite:r,removeFavorite:c}=h(),i=()=>{l(!n)};return o.jsxs("div",{children:[o.jsx(S,{children:"Favorites campers"}),r.length===0?o.jsx(F,{children:"No favorite cemper selected."}):o.jsx("ul",{children:r.map(t=>o.jsxs(m,{children:[o.jsxs(w,{children:[o.jsx("img",{src:t.gallery[0],alt:"cemper"}),o.jsxs("div",{children:[o.jsx(u,{children:t.name}),o.jsx($,{children:o.jsxs("div",{children:[o.jsxs(R,{children:[o.jsx(g,{children:o.jsx("use",{href:`${a}#Rating`})})," ",o.jsx("p",{children:t.rating}),o.jsxs("p",{children:["(",t.reviews.reduce((x,d)=>x+d.reviewer_rating,0)," ","Reviews)"]})]}),o.jsxs(C,{children:[" ",o.jsx(f,{children:o.jsx("use",{href:`${a}#icon-map`})}),o.jsx(B,{children:o.jsxs("p",{children:[" ",t.location]})})]})]})}),o.jsx(b,{type:"button",onClick:i,children:"Show more"})]})]}),o.jsx(y,{type:"button",onClick:()=>c(t),children:"Remove"}),o.jsx(k,{children:t.price}),n&&o.jsx(j,{onClose:i,children:o.jsx(v,{id:t._id,toggleModal:i})})]},t._id))})]})},I=()=>o.jsx(_,{});export{I as default};
