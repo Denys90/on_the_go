5f5298417a0ffb6392267e9daa2da96f1f602ef7
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2024-04-29 19:49:13 +0200

    added styles for filter

--- src/components/Filters/Location/Filter/Filter.jsx
+++ src/components/Filters/Location/Filter/Filter.jsx
@@ -1,12 +1,19 @@
 import { Formik, Form } from 'formik';
 import {
   CheckBoxButton,
+  CheckBoxButtonVan,
   CheckBoxInput,
   CheckBoxLabel,
   ContainerForEquipment,
+  Equipment,
+  EquipmentBox,
   FirstTitle,
   SecondTitle,
+  SvgStyles,
+  SvgStylesVan,
+  VehicleBox,
 } from './Filter.styled';
+import svg from 'assets/icons/symbol-defs.svg';
 
 export const Filter = () => {
   const initialValues = {
@@ -15,6 +22,9 @@ export const Filter = () => {
     option3: false,
     option4: false,
     option5: false,
+    Van: false,
+    Fully: false,
+    Alcove: false,
   };
 
   return (
@@ -25,55 +35,131 @@ export const Filter = () => {
         <Formik initialValues={initialValues} onSubmit={() => {}}>
           {({ values, setFieldValue }) => (
             <Form>
-              <CheckBoxButton>
-                <CheckBoxInput
-                  type="checkbox"
-                  name="option1"
-                  checked={values.option1}
-                  onChange={(e) => setFieldValue('option1', e.target.checked)}
-                />
-                <CheckBoxLabel option={values.option1}>Option 1</CheckBoxLabel>
-              </CheckBoxButton>
+              <EquipmentBox>
+                <CheckBoxButton>
+                  <CheckBoxInput
+                    type="checkbox"
+                    name="option1"
+                    checked={values.option1}
+                    onChange={(e) => setFieldValue('option1', e.target.checked)}
+                  />
+                  <CheckBoxLabel option={values.option1}>
+                    <SvgStyles>
+                      <use href={`${svg}#icon-wind`}></use>
+                    </SvgStyles>
+                    <Equipment>AC</Equipment>
+                  </CheckBoxLabel>
+                </CheckBoxButton>
 
-              <CheckBoxButton>
-                <CheckBoxInput
-                  type="checkbox"
-                  name="option2"
-                  checked={values.option2}
-                  onChange={(e) => setFieldValue('option2', e.target.checked)}
-                />
-                <CheckBoxLabel option={values.option2}>Option 2</CheckBoxLabel>
-              </CheckBoxButton>
+                <CheckBoxButton>
+                  <CheckBoxInput
+                    type="checkbox"
+                    name="option2"
+                    checked={values.option2}
+                    onChange={(e) => setFieldValue('option2', e.target.checked)}
+                  />
+                  <CheckBoxLabel option={values.option2}>
+                    <SvgStyles>
+                      <use href={`${svg}#icon-conect`}></use>
+                    </SvgStyles>
+                    <Equipment>Automatic</Equipment>
+                  </CheckBoxLabel>
+                </CheckBoxButton>
 
-              <CheckBoxButton>
-                <CheckBoxInput
-                  type="checkbox"
-                  name="option3"
-                  checked={values.option3}
-                  onChange={(e) => setFieldValue('option3', e.target.checked)}
-                />
-                <CheckBoxLabel option={values.option3}>Option 3</CheckBoxLabel>
-              </CheckBoxButton>
+                <CheckBoxButton>
+                  <CheckBoxInput
+                    type="checkbox"
+                    name="option3"
+                    checked={values.option3}
+                    onChange={(e) => setFieldValue('option3', e.target.checked)}
+                  />
+                  <CheckBoxLabel option={values.option3}>
+                    <SvgStyles>
+                      <use href={`${svg}#icon-fork`}></use>
+                    </SvgStyles>
+                    <Equipment>Kitchen</Equipment>
+                  </CheckBoxLabel>
+                </CheckBoxButton>
 
-              <CheckBoxButton>
-                <CheckBoxInput
-                  type="checkbox"
-                  name="option4"
-                  checked={values.option4}
-                  onChange={(e) => setFieldValue('option4', e.target.checked)}
-                />
-                <CheckBoxLabel option={values.option4}>Option 4</CheckBoxLabel>
-              </CheckBoxButton>
+                <CheckBoxButton>
+                  <CheckBoxInput
+                    type="checkbox"
+                    name="option4"
+                    checked={values.option4}
+                    onChange={(e) => setFieldValue('option4', e.target.checked)}
+                  />
+                  <CheckBoxLabel option={values.option4}>
+                    <SvgStyles>
+                      <use href={`${svg}#icon-tv`}></use>
+                    </SvgStyles>
+                    <Equipment>TV</Equipment>
+                  </CheckBoxLabel>
+                </CheckBoxButton>
 
-              <CheckBoxButton>
-                <CheckBoxInput
-                  type="checkbox"
-                  name="option5"
-                  checked={values.option5}
-                  onChange={(e) => setFieldValue('option5', e.target.checked)}
-                />
-                <CheckBoxLabel option={values.option5}>Option 5</CheckBoxLabel>
-              </CheckBoxButton>
+                <CheckBoxButton>
+                  <CheckBoxInput
+                    type="checkbox"
+                    name="option5"
+                    checked={values.option5}
+                    onChange={(e) => setFieldValue('option5', e.target.checked)}
+                  />
+                  <CheckBoxLabel option={values.option5}>
+                    <SvgStyles>
+                      <use href={`${svg}#icon-shower`}></use>
+                    </SvgStyles>
+                    <Equipment>Shower/WC</Equipment>
+                  </CheckBoxLabel>
+                </CheckBoxButton>
+              </EquipmentBox>
+
+              {/* Vehicle type */}
+              <SecondTitle>Vehicle type</SecondTitle>
+              <VehicleBox>
+                <CheckBoxButtonVan>
+                  <CheckBoxInput
+                    type="checkbox"
+                    name="Van"
+                    checked={values.Van}
+                    onChange={(e) => setFieldValue('Van', e.target.checked)}
+                  />
+                  <CheckBoxLabel option={values.Van}>
+                    <SvgStylesVan>
+                      <use href={`${svg}#icon-van1`}></use>
+                    </SvgStylesVan>
+                    <Equipment>Van</Equipment>
+                  </CheckBoxLabel>
+                </CheckBoxButtonVan>
+
+                <CheckBoxButtonVan>
+                  <CheckBoxInput
+                    type="checkbox"
+                    name="Fully"
+                    checked={values.Fully}
+                    onChange={(e) => setFieldValue('Fully', e.target.checked)}
+                  />
+                  <CheckBoxLabel option={values.Fully}>
+                    <SvgStylesVan>
+                      <use href={`${svg}#icon-van2`}></use>
+                    </SvgStylesVan>
+                    <Equipment>Fully Integrated</Equipment>
+                  </CheckBoxLabel>
+                </CheckBoxButtonVan>
+
+                <CheckBoxButtonVan>
+                  <CheckBoxInput
+                    type="checkbox"
+                    name="Alcove"
+                    checked={values.Alcove}
+                    onChange={(e) => setFieldValue('Alcove', e.target.checked)}
+                  />
+                  <CheckBoxLabel option={values.Alcove}>
+                    <SvgStylesVan>
+                      <use href={`${svg}#icon-van3`}></use>
+                    </SvgStylesVan>
+                    <Equipment>Alcove</Equipment>
+                  </CheckBoxLabel>
+                </CheckBoxButtonVan>
+              </VehicleBox>
             </Form>
           )}
         </Formik>
--- src/components/Filters/Location/Filter/Filter.styled.js
+++ src/components/Filters/Location/Filter/Filter.styled.js
@@ -1,9 +1,25 @@
 import styled from '@emotion/styled';
 import { theme } from 'styles/theme';
+const CheckBoxCommon = `
+  display: inline-block;
+  cursor: pointer;
 
+`;
+
+// ==========================================
 export const ContainerForEquipment = styled.div`
   width: 360px;
   display: inline-flex;
+  border-top: 2px solid ${theme.colors.grayScroll};
+  padding-top: 24px;
+  margin-right: 64px;
+`;
+export const EquipmentBox = styled.div`
+  margin-bottom: 32px;
+`;
+export const VehicleBox = styled.div`
+  border-top: 2px solid ${theme.colors.grayScroll};
+  padding-top: 24px;
 `;
 
 export const FirstTitle = styled.h2`
@@ -12,6 +28,7 @@ export const FirstTitle = styled.h2`
   line-height: 24px;
   letter-spacing: 0%;
   text-align: left;
+  margin-bottom: 14px;
 `;
 export const SecondTitle = styled.h3`
   font-size: 20px;
@@ -19,29 +36,69 @@ export const SecondTitle = styled.h3`
   line-height: 24px;
   letter-spacing: 0%;
   text-align: left;
+  margin-bottom: 24px;
 `;
 
 export const CheckBoxButton = styled.label`
-  display: inline-block;
-  /* margin-right: 10px; */
-
-  cursor: pointer;
-`;
-
-export const CheckBoxLabel = styled.span`
-  display: inline-block;
+  ${CheckBoxCommon}
   margin-right: 10px;
-  width: 110px;
-  height: 95px;
-  border: 2px solid ${theme.colors.lightGray};
-  border-radius: 10px;
-  border-color: ${(props) =>
-    props.option ? `${theme.colors.red}` : `${theme.colors.grayScroll}`};
+  margin-bottom: 10px;
+  &:nth-of-type(3n) {
+    margin-right: 0;
+  }
+  &:last-of-type,
+  &:nth-last-of-type(2) {
+    margin-bottom: 0;
+  }
 `;
+export const CheckBoxButtonVan = styled.label`
+  ${CheckBoxCommon}
+  margin-right: 10px;
+  &:nth-last-of-type(1) {
+    margin-right: 0;
+  }
+`;
+
 export const CheckBoxInput = styled.input`
   position: absolute !important;
   opacity: 0;
   pointer-events: none;
   width: 0;
   height: 0;
+`;
+
+export const CheckBoxLabel = styled.span`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 110px;
+  height: 95px;
+  border-radius: 10px;
+  border: 2px solid ${theme.colors.black};
+  border-color: ${(props) =>
+    props.option ? `${theme.colors.red}` : `${theme.colors.grayScroll}`};
+`;
+
+export const SvgStyles = styled.svg`
+  display: block;
+  width: 28px;
+  height: 24px;
+  stroke: #111;
+  margin-bottom: 10px;
+`;
+export const SvgStylesVan = styled.svg`
+  display: block;
+  width: 40px;
+  height: 28px;
+  margin-bottom: 9px;
+  stroke: #111;
+`;
+export const Equipment = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 20px;
+  letter-spacing: 0%;
+  text-align: center;
 `;
