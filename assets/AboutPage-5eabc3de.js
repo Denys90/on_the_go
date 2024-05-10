1b2d6a7d8e69ebd4f5dfbe84541620e58fdd6ee4
Denys90 <112502591+Denys90@users.noreply.github.com>
2024-05-10 18:16:50 +0000

    Deploying to gh-pages from @ Denys90/on_the_go@2ab6049370516440bbe96494979aae4b63086e3a 🚀

--- assets/AboutPage-5eabc3de.js
+++ assets/AboutPage-5eabc3de.js
@@ -1,4 +1,4 @@
-import{n as Se,t as qe,s as yt,r as it,j as X}from"./index-0bd19ef1.js";const St=Se.div`
+import{n as Se,t as qe,s as yt,r as it,j as X}from"./index-afeb5ed4.js";const St=Se.div`
   padding-top: 100px;
 `,xt=Se.div`
   display: grid;
